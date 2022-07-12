81a2c0944e6c7c1f985ed2141e26f726332aedca
mehdi <mehdi.shokohifar@gmail.com>
2022-07-12 16:55:33 +0430

    update gitignore

--- src/route/index.ts
+++
@@ -1,6 +0,0 @@
-import contextRoute from '/media/DDrive/Lab/fastify-ts/src/route/context'
-import testRoute from '/media/DDrive/Lab/fastify-ts/src/route/test'
-export default function registerRoutes (server){
- server.register(contextRoute,{ prefix: '/context' })
- server.register(testRoute,{ prefix: '/test' })
-}
